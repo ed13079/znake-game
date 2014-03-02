7920ff36ab648714cddaebcf7ac94d029a10ed07
edbert <edbert@edbert-pc>
2014-03-02 17:19:56 +0700

    Pengubahan kelas ZnakeConstants

--- src/main/java/ac/id/itb/tgs2/znake/ZnakeConstants.java
+++ src/main/java/ac/id/itb/tgs2/znake/ZnakeConstants.java
@@ -13,13 +13,14 @@ package ac.id.itb.tgs2.znake;
 public interface ZnakeConstants {
     
     // Board
-    static final int BOARD_WIDTH = 20;
-    static final int BOARD_HEIGHT = 20;
+    static final int BOARD_WIDTH = 50;
+    static final int BOARD_HEIGHT = 50;
     static final int CELL_WIDTH = 10;
     static final int CELL_HEIGHT = 10;
     
     // Znake
-    static final int DEFAULT_SNAKE_LENGTH = 3;
+    static final int DEFAULT_SNAKE_LENGTH = 5;
+    static final int DEFAULT_SPEED_1 = 500;
     
     // Direction
     static final int NORTH = 0;
