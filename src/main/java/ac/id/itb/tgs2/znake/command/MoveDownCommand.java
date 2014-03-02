9e4ea7af6b5c9bdc63a1c78559046e8a456e2349
edbert <edbert@edbert-pc>
2014-03-02 16:13:38 +0700

    Penambahan command move: move up, move right, move down, move left

---
+++ src/main/java/ac/id/itb/tgs2/znake/command/MoveDownCommand.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ac.id.itb.tgs2.znake.command;
+
+import ac.id.itb.tgs2.znake.utilities.ZnakeOperation;
+
+/**
+ *
+ * @author edbert
+ */
+public class MoveDownCommand extends MoveCommand {
+    
+    public MoveDownCommand(ZnakeOperation operation) {
+        super(operation);
+    }
+    
+    @Override
+    public void execute() {
+        operation.moveDown();
+    }
+    
+}
