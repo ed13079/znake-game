77f227d12af97b2d2ecf403fc9909a903ff3b13b
Orang Itu <perfectcengos@gmail.com>
2014-03-04 12:27:22 +0700

    Membersihkan source code Main

--- src/main/java/itb/rpl/ppl/tgs2/znake/Main.java
+++ src/main/java/itb/rpl/ppl/tgs2/znake/Main.java
@@ -1,7 +1,5 @@
 package itb.rpl.ppl.tgs2.znake;
 
-import itb.rpl.ppl.tgs2.znake.model.food.Food;
-import itb.rpl.ppl.tgs2.znake.model.food.FoodFactory;
 import itb.rpl.ppl.tgs2.znake.view.ZnakeView;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
@@ -13,14 +11,6 @@ import javax.swing.UIManager;
 public class Main {
 
     public static void main(String[] args) {
-        // Coba create food pake teknik advance: Class.forName() dan Class.newInstance()
-        // 3:)
-//        Food f = FoodFactory.createDefaultFood();
-//        System.out.println(f);
-//
-//        f = FoodFactory.createExtraFood();
-//        System.out.println(f);
-
         // Run aplikasi yang sesungguhnya
         java.awt.EventQueue.invokeLater(new Runnable() {
             @Override
